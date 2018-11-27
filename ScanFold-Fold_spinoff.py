22aaa871bc24edddc6009d7b2b843713f37f6b2c
rjandr <randrews@iastate.edu>
2018-11-26 20:54:42 -0600

    changing requests

--- ScanFold-Fold_spinoff.py
+++ ScanFold-Fold_spinoff.py
@@ -1006,9 +1006,7 @@ if competition == 1:
     # os.system(str("ct2dot "+output+"below_mean_"+str(round(meanz, 2))+".ct 1 "+output+"below_mean_"+str(round(meanz, 2))+".dbn"))
     # os.system(str("ct2dot "+output+"1sd_below_mean_"+str(round(one_sig_below, 2))+".ct 1 "+output+"1sd_below_mean_"+str(round(one_sig_below, 2))+".dbn"))
     # os.system(str("ct2dot "+output+"2sd_below_mean_"+str(round(two_sig_below, 2))+".ct 1 "+output+"2sd_below_mean_"+str(round(two_sig_below, 2))+".dbn"))
-    r = requests.get(str(callbackurl)+"/"+str(nodeid)+"/0")
-    r = requests.get(str(callbackurl)+"/"+str(nodeid))
-
-    print(r)
+    url = str(callbackurl+"/"+str(nodeid)+"/0")
+    response = requests.get(url)
 
     print("ScanFold-Fold complete, find results in...")
