4e4a3fa1891494c8408adff0c979367a79c42fc8
Levi Baber <baberlevi@gmail.com>
2018-12-26 20:46:51 -0600

    stop trying to verify ssl, even though it should be fine

--- ScanFold-Scan_Webserver.py
+++ ScanFold-Scan_Webserver.py
@@ -528,7 +528,7 @@ write_fai(seq, fasta_index, name)
 
 if split == "off":
     url = str(callbackurl+"/"+str(nodeid)+"/0")
-    response = requests.get(url)
+    response = requests.get(url, verify=False)
     # print(url)
 
 if split == "on":
